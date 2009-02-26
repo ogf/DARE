247e06307d8ae5b170625347546dfd52a994fac2
Oscar Gonzalez <ogfernandez@gmail.com>
2009-02-26 13:31:31 +0100

    functioning ant build

---
+++ src/main/java/prueba/Bla.java
@@ -0,0 +1,16 @@
+package prueba;
+
+public class Bla {
+	
+	public static void main(String[] args) {
+		System.out.println("probando");
+	}
+	
+	public Bla() {
+	}
+	
+	public String doBla(){
+		return "bla";
+	}
+
+}
