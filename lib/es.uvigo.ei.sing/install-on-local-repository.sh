0f24b1fb88dcbc36368c4ace5aca3c27f227bf49
Óscar González Fernández <ogonzalez@igalia.com>
2011-03-14 13:23:18 +0100

    Publish artifacts in local Maven repository
    Thus the artifacts can be used from clojure Leiningen projects, since
    Leiningen uses maven for dependency grabing.
    
    Because Maven doesn't support version ranges in local repositories,
    the precise versions have to be specified in the ivy.xml files. An
    alternative would be to use a branch of Cake that has ivy support.

---
+++ lib/es.uvigo.ei.sing/install-on-local-repository.sh
@@ -0,0 +1,1 @@
+mvn install:install-file -DgroupId=es.uvigo.ei.sing -DartifactId=stringeditor -Dversion=1.0 -Dpackaging=jar -Dfile=`dirname $0`/stringeditor-1.0.jar -DpomFile=`dirname $0`/stringeditor.pom
