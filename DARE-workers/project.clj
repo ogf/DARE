a19fd7d43c424d4e76003def830ac383a5197dce
Óscar González Fernández <ogfernandez@gmail.com>
2012-01-10 20:58:21 +0100

    Use the latest stable aleph version

--- DARE-workers/project.clj
+++ DARE-workers/project.clj
@@ -2,7 +2,7 @@
   :description "Responsible of receiving executions requests and executing them"
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [aleph "[0.2.0-alpha,0.2.0)"]
+                 [aleph "0.2.0"]
                  [es.uvigo.ei.sing/minilanguage "0.1"]
                  [es.uvigo.ei.sing/DARE-util "0.1"]
                  [es.uvigo.ei.sing/stringeditor "1.0"]
