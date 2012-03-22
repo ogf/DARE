e05bf60becf44063065c901bb077a25be0266c66
Óscar González Fernández <ogfernandez@gmail.com>
2012-03-22 17:40:56 +0100

    Change organization and some names

--- minilanguage/src/main/resources/es/uvigo/ei/sing/stringeditor/transformer_spec.rb
+++ minilanguage/src/main/resources/es/uvigo/ei/sing/stringeditor/transformer_spec.rb
@@ -1,159 +1,168 @@
 require 'transformer'
 
-describe "Transformers" do
+describe "aAUTOMATOR minilanguage" do
 
-  it "description shouldn't be changed" do
-    t = SimpleTransformer.new :description => "blabla"
-    lambda{t.description = "blabla"}.should raise_error
-  end
+  describe "Transformer" do
 
-  it "should accept description" do
-    t = SimpleTransformer.new :description => "blabla"
-    t.description.should == "blabla"
-  end
-
-  it "should have transformer_class name as default value for description" do
-    t = SimpleTransformer.new({})
-    t.description.should == "SimpleTransformer"
-  end
-
-  it "should accept branchtype parameter" do
-    t = SimpleTransformer.new :branchtype => "BRANCH_DUPLICATED"
-    t.branchtype.should == :BRANCH_DUPLICATED
-  end
-
-  it "should have branchtype CASCADE as default" do
-    t = SimpleTransformer.new({})
-    t.branchtype.should == :CASCADE
-  end
-
-  it "should accept parameter branchmergemode" do
-    t = SimpleTransformer.new :branchmergemode =>:ORDERED
-    t.branchmergemode.should == :ORDERED
-  end
-
-  it "should have branchmergemode SCATTERED as default" do
-    t = SimpleTransformer.new({})
-    t.branchmergemode.should == :SCATTERED
-  end
-
-  it "should accept parameter loop" do
-    t = SimpleTransformer.new :loop => true
-    t.loop?.should be_true
-  end
-
-  it "should keep parameters" do
-    t = SimpleTransformer.new :other => "bla"
-    t.params[:other].should_not be_nil
-  end
-
-  it "should let add children" do
-    t = SimpleTransformer.new :loop => true
-    child = SimpleTransformer.new :loop => true
-    t.add_child child
-    t.children.should == [child]
-  end
-
-  it "must have an empty list as children initially" do
-    t = SimpleTransformer.new :loop => true
-    t.children.should_not be_nil
-  end
-
-  it "should not let modify original children collection" do
-    t = SimpleTransformer.new :loop => true
-    t.add_child(SimpleTransformer.new( :loop => true))
-    t.children.clear
-    t.children.length.should == 1
-  end
-
-  shared_examples_for "transformer invariants" do
-    it "should have a not nil description" do
-      @transformer.description.should_not be_nil
+    it "should accept description" do
+      t = SimpleTransformer.new :description => "blabla"
+      t.description.should == "blabla"
     end
 
-    it "should have a not nil transformer_class" do
-      @transformer.transformer_class.should_not be_nil
+    it "description shouldn't be allowed to be changed once set" do
+      t = SimpleTransformer.new :description => "blabla"
+      lambda{t.description = "blabla"}.should raise_error
     end
 
-    it "should have a not nil branchtype" do
-      @transformer.branchtype.should_not be_nil
+    it "should have transformer_class name as default value for description" do
+      t = SimpleTransformer.new({})
+      t.description.should == "SimpleTransformer"
     end
 
-    it "should have a not nil branchmergemode" do
-      @transformer.branchmergemode.should_not be_nil
+    it "should accept branchtype parameter" do
+      t = SimpleTransformer.new :branchtype => "BRANCH_DUPLICATED"
+      t.branchtype.should == :BRANCH_DUPLICATED
     end
 
-    def mock_children(transformer, mock = mock("child serializer"))
-      attributes_mock = mock("attributes")
-      mock.stub!(:attributes).and_return attributes_mock
-      attributes_mock.should_receive("[]=").with('class', @transformer.transformer_class.to_s)
-      attributes_mock.should_receive("[]=").
-        with('branchtype', @transformer.branchtype.to_s)
-      attributes_mock.should_receive("[]=").
-        with('branchmergemode', @transformer.branchmergemode.to_s)
-      attributes_mock.should_receive("[]=").with('loop', @transformer.loop?.to_s)
-      pos = -1
-      mock.should_receive("add_element").exactly(transformer.children.length).and_return{
-                                           pos = pos + 1
-                                           mock_children(transformer.children[pos])
-                                          }
-      mock.should_receive("add_param").exactly(transformer.params.size)
-      mock
+    it "should have branchtype CASCADE as default" do
+      t = SimpleTransformer.new({})
+      t.branchtype.should == :CASCADE
     end
 
-    it "should traverse tranformer" do
-      mock = mock("top serializer")
-      @transformer.fill_transformer_element mock_children(@transformer, mock)
-    end
-  end
-
-  describe "children nested" do
-    before(:all) do
-      @transformer = PatternMatcher.new :pattern => 'bla'
-      child = PatternMatcher.new :pattern => %q{<li><a href="noticia.asp\?idnoticia=.*?">(.*?)</a>},
-                                 :dotAll => true
-      @transformer.add_child child
-      doc = @transformer.convert_to_xml
-      doc.should_not be_nil
-      # doc.write($stdout,4)
-    end
-    it_should_behave_like "transformer invariants"
-  end
-
-  describe PatternMatcher do
-
-    before(:all) do
-      @transformer = PatternMatcher.new :pattern => 'bla'
+    it "should accept parameter branchmergemode" do
+      t = SimpleTransformer.new :branchmergemode =>:ORDERED
+      t.branchmergemode.should == :ORDERED
     end
 
-    it_should_behave_like "transformer invariants"
-
-    it "should have a fixed transformer class" do
-      p = PatternMatcher.new :pattern => 'bla'
-      p.transformer_class.should == :PatternMatcher
+    it "should have branchmergemode SCATTERED as default" do
+      t = SimpleTransformer.new({})
+      t.branchmergemode.should == :SCATTERED
     end
 
-    it "should require parameter pattern" do
-      lambda {
-        p = PatternMatcher.new({})
-      }.should raise_error(ArgumentError, "pattern parameter is required")
+    it "should accept parameter loop" do
+      t = SimpleTransformer.new :loop => true
+      t.loop?.should be_true
     end
 
-    it "should read parameter pattern" do
-      p = PatternMatcher.new :pattern =>  'bla'
-      p.pattern.should == 'bla'
+    it "should keep parameters" do
+      t = SimpleTransformer.new :other => "bla"
+      t.params[:other].should_not be_nil
     end
 
-    it "should let specify pattern as just one param" do
-      p = PatternMatcher.new('bla')
-      p.pattern.should == 'bla'
+    it "should let add children" do
+      t = SimpleTransformer.new :loop => true
+      child = SimpleTransformer.new :loop => true
+      t.add_child child
+      t.children.should == [child]
     end
 
-    it "should let specify additional params" do
-      p = PatternMatcher.new :pattern => 'bla', :bla => "a"
-      p.params[:bla].should == "a"
+    it "must have an empty list as children initially" do
+      t = SimpleTransformer.new :loop => true
+      t.children.should_not be_nil
     end
 
+    it "should not let modify original children collection" do
+      t = SimpleTransformer.new :loop => true
+      t.add_child(SimpleTransformer.new( :loop => true))
+      t.children.clear
+      t.children.length.should == 1
+    end
+
+    shared_examples_for "invariants for a Transformer" do
+      it "should have a not nil description" do
+        @transformer.description.should_not be_nil
+      end
+
+      it "should have a not nil transformer_class" do
+        @transformer.transformer_class.should_not be_nil
+      end
+
+      it "should have a not nil branchtype" do
+        @transformer.branchtype.should_not be_nil
+      end
+
+      it "should have a not nil branchmergemode" do
+        @transformer.branchmergemode.should_not be_nil
+      end
+
+      def mock_children(transformer, mock = mock("child serializer"))
+        attributes_mock = mock("attributes")
+        mock.stub!(:attributes).and_return attributes_mock
+        attributes_mock.should_receive("[]=").with('class', @transformer.transformer_class.to_s)
+        attributes_mock.should_receive("[]=").
+          with('branchtype', @transformer.branchtype.to_s)
+        attributes_mock.should_receive("[]=").
+          with('branchmergemode', @transformer.branchmergemode.to_s)
+        attributes_mock.should_receive("[]=").with('loop', @transformer.loop?.to_s)
+        pos = -1
+        mock.should_receive("add_element").exactly(transformer.children.length).and_return{
+                                             pos = pos + 1
+                                             mock_children(transformer.children[pos])
+                                            }
+        mock.should_receive("add_param").exactly(transformer.params.size)
+        mock
+      end
+
+      it "should traverse tranformer" do
+        mock = mock("top serializer")
+        @transformer.fill_transformer_element mock_children(@transformer, mock)
+      end
+    end
+
+    describe "can have other transformers as children" do
+      before(:all) do
+        @transformer = PatternMatcher.new :pattern => 'bla'
+        @child = PatternMatcher.new :pattern => %q{<li><a href="noticia.asp\?idnoticia=.*?">(.*?)</a>},
+                                   :dotAll => true
+        @transformer.add_child @child
+        doc = @transformer.convert_to_xml
+        doc.should_not be_nil
+        # doc.write($stdout,4)
+      end
+
+      it_should_behave_like "invariants for a Transformer"
+
+      it "should report the added children" do
+        @transformer.children.size.should == 1
+        @transformer.children[0].should == @child
+      end
+    end
+
+    describe PatternMatcher do
+
+      before(:all) do
+        @transformer = PatternMatcher.new :pattern => 'bla'
+      end
+
+      it_should_behave_like "invariants for a Transformer"
+
+      it "should have a fixed transformer class" do
+        p = PatternMatcher.new :pattern => 'bla'
+        p.transformer_class.should == :PatternMatcher
+      end
+
+      it "should require parameter pattern" do
+        lambda {
+          p = PatternMatcher.new({})
+        }.should raise_error(ArgumentError, "pattern parameter is required")
+      end
+
+      it "should read parameter pattern" do
+        p = PatternMatcher.new :pattern =>  'bla'
+        p.pattern.should == 'bla'
+      end
+
+      it "should let specify pattern as just one param" do
+        p = PatternMatcher.new('bla')
+        p.pattern.should == 'bla'
+      end
+
+      it "should let specify additional params" do
+        p = PatternMatcher.new :pattern => 'bla', :bla => "a"
+        p.params[:bla].should == "a"
+      end
+
+    end
   end
 
   describe Language do
