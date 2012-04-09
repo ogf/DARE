7c5d8184457992f52a000eb95c489c6f8fbe4246
Óscar González Fernández <ogfernandez@gmail.com>
2012-04-09 14:34:13 +0200

    Add "portada"

--- docs/join_pdfs.sh
+++ docs/join_pdfs.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "$0" )" && pwd )"
-texexec --pdfcopy --nobanner --result=$DIR/final.pdf $DIR/proyecto.pdf $DIR/anteproxecto/anteproyecto-v1.pdf
+texexec --pdfcopy --nobanner --result=$DIR/final.pdf $DIR/portada.pdf $DIR/proyecto.pdf $DIR/anteproxecto/anteproyecto-v1.pdf
